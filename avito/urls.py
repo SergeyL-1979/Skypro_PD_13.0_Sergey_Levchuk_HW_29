9d6916f88d9f8bd9286440501c880bbe864b7b05
sergd <videomc1979@gmail.com>
2023-03-21 00:00:28 +0300

    HW_29

--- avito/urls.py
+++ avito/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from ads import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', views.root),
+    path('api-auth/', include('rest_framework.urls')),
+
+    path('ad/', include('ads.urls')),
+    path('cat/', include('category.urls')),
+    path('users/', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
