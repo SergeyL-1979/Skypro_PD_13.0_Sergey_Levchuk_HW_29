182d9679ff0b04ccbd4c44584947a997a9ab32b6
sergd <videomc1979@gmail.com>
2023-03-21 12:49:18 +0300

    HW_29 added serializer to locations

--- users/urls.py
+++ users/urls.py
@@ -11,14 +11,13 @@ urlpatterns = [
     path("by_user/", views.UserAdsDetailView.as_view(), name="user_ads_count"),
 
     path("location/", views.LocationListAPIView.as_view(), name="location_list"),
-    # path("location/", views.LocationListView.as_view(), name="location_list"),
-    path("<int:pk>/location/", views.LocationDetailAPIView.as_view(), name="location_detail"),
+    path("location/<int:pk>/", views.LocationDetailAPIView.as_view(), name="location_detail"),
     path("location/create/", views.LocationCreateAPIView.as_view(), name="location_create"),
     path("location/<int:pk>/update/", views.LocationUpdateAPIView.as_view(), name="location_update"),
     path("location/<int:pk>/delete/", views.LocationDeleteAPIView.as_view(), name="location_update"),
 
 
-
+    # path("location/", views.LocationListView.as_view(), name="location_list"),
     # path("<int:pk>/location/", views.LocationDetailView.as_view(), name="location_detail"),
     # TO=== не выполнено LocationDetailView
     # path('location/<int:pk>/', views.LocationDetailView.as_view(), name='location_list'),
